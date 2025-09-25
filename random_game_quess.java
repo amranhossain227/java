fdcb2f11300730d54dc68f4bd1ed6eb77eb57d6a
amranhossain227 <amranhossain227@gmail.com>
2025-09-25 16:48:02 +0600

    add the method of that same proess

--- random_game_quess.java
+++ random_game_quess.java
@@ -1,28 +1,59 @@
 import java.util.Scanner;
 
 public class random_game_quess {
+
+    static void game(){
+
+        String m="y";
+        Scanner k=new Scanner(System.in);
+        while(m.equals("y")){
+
+            System.out.println("guess the number 0 to 9");
+            int i=Integer.parseInt(k.nextLine());
+            int number=(int)(Math.random()*10); 
+            if (number==i){
+                System.out.println("you win and the number is " +number);
+            }else{
+                System.out.println("try again the number is " +number +" you give the number "+i);
+            }
+            System.out.println("do you want to play again? (y/n)");
+            m=k.nextLine();
+
+
+        }
+    }
+
+
+
+
+
+
+
     public static void main(String[] args){
         
-        String m = "y";
-        Scanner k = new Scanner(System.in);
+        // String m = "y";
+        // Scanner k = new Scanner(System.in);
 
-        while(m.equals("y")){  // <-- FIXED
+        // while(m.equals("y")){  // <-- FIXED
 
-            System.out.println("Guess the number between 0 to 9");
-            int i = Integer.parseInt(k.nextLine());
-            int number = (int)(Math.random() * 10);
+        //     System.out.println("Guess the number between 0 to 9");
+        //     int i = Integer.parseInt(k.nextLine());
+        //     int number = (int)(Math.random() * 10);
 
-            if (number == i){
-                System.out.println("You are right! " + number);
-            } else {
-                System.out.println("Try again! The number was " + number);
-            }
+        //     if (number == i){
+        //         System.out.println("You are right! " + number);
+        //     } else {
+        //         System.out.println("Try again! The number was " + number);
+        //     }
 
-            // Ask again after each round
-            System.out.println("Do you want to play again? (y/n)");
-            m = k.nextLine();  // reuse same Scanner
-        }
+        //     // Ask again after each round
+        //     System.out.println("Do you want to play again? (y/n)");
+        //     m = k.nextLine();  // reuse same Scanner
+        // }
 
-        System.out.println("Thank you for playing!");
+        // System.out.println("Thank you for playing!");
+
+
+        game();
     }
 }
