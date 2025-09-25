6931a319fb62a1351203f89259ec5f16cdff55ac
amranhossain227 <amranhossain227@gmail.com>
2025-09-25 19:31:32 +0600

    object create and call the method from other class

---
+++ first.java
@@ -0,0 +1,10 @@
+class first{
+    public void display() {
+        System.out.println("This is the first class.");
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
